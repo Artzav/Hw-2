a833cdb6b5cc5ab62b865ce09843c7c06d301527
Artzav <53013268+Artzav@users.noreply.github.com>
2019-12-06 16:33:52 +0200

    Add files via upload

--- PositiveSum.java
+++ PositiveSum.java
@@ -3,12 +3,12 @@ package com.homework;
 public class PositiveSum {
     public static void main(String[] args) {
         int[] array = {5, -10, 7, -2, 36, -30};
-        int Sum = 0;
+        int sumOfPositiveNumbers = 0;
         for (int i : array) {
             if (i > 0) {
-                Sum += i;
+                sumOfPositiveNumbers += i;
             }
         }
-        System.out.println(Sum);
+        System.out.println(sumOfPositiveNumbers);
     }
 }
--- ReserveSortedArray.java
+++ ReserveSortedArray.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 public class ReserveSortedArray {
     public static void main(String[] args) {
         Integer[] numbers = {2, 3, 1, 7, 11};
+        
         Arrays.sort(numbers, Collections.reverseOrder());
+
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
